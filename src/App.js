e79a90347638b679a7974294695321b92a44188a
Redfoxbox <kerecsen.gergo@gmail.com>
2022-03-02 16:12:15 +0100

    auh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import env from './config/env';
 import { Admin, Resource } from 'react-admin';
 import lb4Provider from 'react-admin-lb4';
+import { authProvider, httpClient } from './Auth';
 import { PeopleCreate, PeopleEdit, PeopleList } from './components/people';
-import {WeightCreate, WeightList, WeightEdit} from './components/weight';
+import { WeightCreate, WeightList, WeightEdit } from './components/weight';
 
 function App() {
   return (
-    <Admin dataProvider={lb4Provider('http://localhost:4000')}>
+    <Admin dataProvider={lb4Provider(env.REACT_APP_API_URL, httpClient)} authProvider={authProvider}>
       <Resource name='people' list={PeopleList} edit={PeopleEdit} create={PeopleCreate} />
-      <Resource name='weights' list={WeightList} edit={WeightEdit} create={WeightCreate}/>
+      <Resource name='weights' list={WeightList} edit={WeightEdit} create={WeightCreate} />
     </Admin>
   );
 }
---
+++ src/config/env.js
@@ -0,0 +1,4 @@
+// utils/env.js
+export default {
+    REACT_APP_API_URL : process.env.REACT_APP_API_URL || "http://localhost:4000"
+  }
